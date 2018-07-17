5f1bb1dd3e23df2c0b99ffae1de3514207864af5
Lam <lam.duong@polku.io>
2018-07-17 23:06:40 +0300

    server eslint config

---
+++ server/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "amd": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016
+    },
+    "rules": {
+        "no-console": 0,
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
